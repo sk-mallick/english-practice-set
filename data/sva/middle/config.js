7db2d18bd93347735ec18b9c563eed72642439eb
Subham Kumar Mallick <83875827+sk-mallick@users.noreply.github.com>
2026-02-10 07:02:33 +0000

    sva middle level question uploded

--- data/sva/middle/config.js
+++ data/sva/middle/config.js
@@ -1,7 +1,7 @@
 export default {
-    order: 3,                    // The number shown on the card (01, 02...)
+    order: 4,                    // The number shown on the card (01, 02...)
     title: "Subject-Verb Agreement",  // The big bold title
-    description: "13 Sets • 15 Questions Each - P", // The small text below
+    description: "4 Sets • 15 Questions Each - M", // The small text below
     engine: "fill",               // "mcq" or "fill" (which HTML file to open)
     icon: "list"                 // "time", "list", "book", or "chat"
 }
